730fae55b4f6335f9cae0713573208b003d9d882
KingsTone <hanatwork21@gmail.com>
2023-04-18 09:56:57 +0900

    init login

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: "../src/main/resources/static",
+  devServer:{
+    // port: 9000,
+    proxy:{
+      'api':{
+        target:'http://localhost:9000',
+        changeOrigin: true // cross origin 허용
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.module
+        .rule('vue')
+        .use('vue-loader')
+        .loader('vue-loader')
+        .tap(options => {
+          // modify the options...
+          return options
+        })
+  }
 })
